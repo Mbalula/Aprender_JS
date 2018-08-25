f8a3829f22d7a7986d8bb17c8d4611f52128beb5
Mbalula <42694067+Mbalula@users.noreply.github.com>
2018-08-25 17:28:59 +0100

    Update 03_3ContFeijoes.js

--- 03_3ContFeijoes.js
+++ 03_3ContFeijoes.js
@@ -4,6 +4,70 @@ Testes JS
 
 //Exercício 03_3 Contando Feijões
 
+var contador=0;
+//var palavraBs="";
+var N=0;
+
+var countBs = function(palavraBs){
+    
+    while (N < palavraBs.length ) {
+    
+        if (palavraBs.charAt (N)== "B"){
+            console.log("LetrasB"+"-"+ palavraBs.charAt (N))
+            N=N+1;
+            contador+=1;
+    }
+        else {
+            console.log("LetrasB"+"-"+ palavraBs.charAt (N));
+            N=N+1;
+            }
+/*    console.log("N1"+"-"+N)
+    console.log("LetrasB"+"-"+ palavraBs.charAt (N))
+    console.log("tamanho de palavras"+"-"+palavraBs.length) */
+
+    }
+console.log("N total letras"+"-"+N)
+return contador
+}
+
+console.log("n de Bs"+"-"+countBs("BBC")); // → 2
+
+/*
+console.log(countBs("BBCB")); // → 2
+console.log("contador"+"-"+contador);
+console.log(N);
+console.log("LetrasB"+"-"+palavraBs.charAt (N));
+console.log("tamanho de palavras"+"-"+palavraBs.length);
+*/
+N=0
+letra_="xxx"
+contador=0
+countChar = function(palavra, letra){
+    letra_=letra
+    while (N < palavra.length ) {
+    
+        if (palavra.charAt (N)== letra){
+            console.log("Letras "+letra_+"-"+ palavra.charAt (N))
+            N=N+1;
+            contador+=1;
+    }
+        else {
+            console.log("Letras "+letra_+"-"+ palavra.charAt (N));
+            N=N+1;
+            }
+/*    console.log("N1"+"-"+N)
+    console.log("LetrasB"+"-"+ palavraBs.charAt (N))
+    console.log("tamanho de palavras"+"-"+palavraBs.length) */
+
+    }
+console.log("N total letras"+"-"+N)
+console.log(letra_)
+return contador
+
+}
+console.log(letra_)
+console.log("n de letras "+letra_+"-"+countChar("kakkerlak", "k")); // → 4
+
 
   document.write (hash,"<br>");
     }
