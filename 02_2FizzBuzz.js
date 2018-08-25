f18dab39ce81c99989bd8859d9d52817a6d14af6
Mbalula <42694067+Mbalula@users.noreply.github.com>
2018-08-25 16:16:22 +0100

    Update 02_2FizzBuzz.js

--- 02_2FizzBuzz.js
+++ 02_2FizzBuzz.js
@@ -4,6 +4,29 @@ Testes JS
 
 //Exercício 2 FizzBuzz
 
+var num=1;
+
+while (num<=100) {
+    if (num%3==0 & num%5==0){
+    console.log(num+"FizzBuzz");
+    num+=1;
+    }
+    else if (num%3==0 || num%5==0) {
+      if (num%3==0) {
+      console.log(num+"Fizz");
+      num+=1;
+      }
+      else {
+      console.log(num+"Buzz");
+      num+=1;
+      }
+    }
+    else {
+    console.log(num);
+    num+=1;
+    }
+}
+
 
   document.write (hash,"<br>");
     }
