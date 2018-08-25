c92cbb5491b6cd81304fc6e75dc2540437508301
Mbalula <42694067+Mbalula@users.noreply.github.com>
2018-08-25 17:10:58 +0100

    Update 03_1Minimo.js

--- 03_1Minimo.js
+++ 03_1Minimo.js
@@ -5,6 +5,19 @@ Testes JS
 //Exercício 03_1 Mínimo
 
 
+// Your code here.
+var min= function (a,b) {
+    if (a<=b)
+        return a
+    else
+        return b
+}
+
+console.log(min(0, 10)); // → 0
+console.log(min(0, -10)); // → -10
+
+
+
   document.write (hash,"<br>");
     }
     nimpress += 1;
