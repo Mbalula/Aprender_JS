ccbfe8b346d4505e28d2ba1d3119ac3b867a3d5f
Mbalula <42694067+Mbalula@users.noreply.github.com>
2018-08-25 17:09:14 +0100

    Update 02_3TabuleiroXadrez.js

--- 02_3TabuleiroXadrez.js
+++ 02_3TabuleiroXadrez.js
@@ -4,6 +4,45 @@ Testes JS
 
 //Exercício 02_3 Tabuleiro de Xadrez
 
+var string="";
+var size=3;
+var colunaI=1;
+var colunaP=1;
+var linha=1;
+
+while (linha<=size){
+    while (colunaI<=size){
+        if(colunaP%2==0){
+        string+="   ";
+        colunaI+=1;
+        colunaP+=1;
+        }
+        else if(colunaP%2!=0){
+        string+=" # ";
+        colunaI+=1;
+        colunaP+=1;
+        }
+//        else{
+//        coluna+=1;
+//        }
+    }
+string+="\n";
+//console.log("colunaI"+coluna);
+//console.log("linhaI"+linha);
+linha+=1;
+if(linha%2==0){
+        colunaI=1;
+        colunaP=0;
+        }
+        else{
+        colunaI=1;
+        colunaP=1;
+        }
+//console.log("colunaD"+coluna);
+//console.log("linhaD"+linha);
+}
+console.log(string);
+alert (string)
 
   document.write (hash,"<br>");
     }
